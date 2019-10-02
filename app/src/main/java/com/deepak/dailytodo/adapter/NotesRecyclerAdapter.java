abeb89f2ca1f76239ff206343b510da67361bf34
deepak kumar <deepak24121994@gmail.com>
2019-10-02 11:45:31 +0530

    Notes Recycler Adapter

---
+++ app/src/main/java/com/deepak/dailytodo/adapter/NotesRecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.deepak.dailytodo.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.deepak.dailytodo.R;
+import com.deepak.dailytodo.models.Note;
+
+import java.util.ArrayList;
+
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+    //to hold notes
+  private   ArrayList<Note>notes=new ArrayList<>();
+
+    public NotesRecyclerAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item,parent,false)
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(notes.get(position).getTitle());
+        holder.timeStamp.setText(notes.get(position).getTimestamp());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title,timeStamp;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.note_title);
+            timeStamp=itemView.findViewById(R.id.note_timestamp);
+        }
+    }
+}
