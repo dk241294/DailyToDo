9a2e757583b61e87ae10021d54cd5950d32c6025
deepak kumar <deepak24121994@gmail.com>
2019-10-10 00:03:43 +0530

    Dao

---
+++ app/src/main/java/com/deepak/dailytodo/persistence/NoteDao.java
@@ -0,0 +1,25 @@
+package com.deepak.dailytodo.persistence;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.deepak.dailytodo.models.Note;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    long[] insertNotes(Note... notes); // [] ---> ...
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes(); //
+
+    @Delete
+    int delete(Note... notes);
+
+    @Update
+    int updateNotes(Note... notes);
+}
